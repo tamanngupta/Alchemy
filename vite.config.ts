7741ec4dc560b866fbec373f9a21fb9c38257190
tamanna gupta <tamannagupta0097@gmail.com>
2026-06-08 03:01:10 +0530

    removed ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
 import { defineConfig } from 'vite';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
-const config = defineConfig({
-  plugins: [tanstackStart()],
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        lesson1: resolve(__dirname, 'public/lessons/lesson1.html'),
-        lesson2: resolve(__dirname, 'public/lessons/lesson2.html'),
-        lesson3: resolve(__dirname, 'public/lessons/3.html'),
-        lesson4: resolve(__dirname, 'public/lessons/lesson4_v3.html'),
-        lesson5: resolve(__dirname, 'public/lessons/lesson5.html'),
-        lesson6: resolve(__dirname, 'public/lessons/lesson6.html'),
-        lesson7: resolve(__dirname, 'public/lessons/lesson6-2.html'),
-      },
-    },
-  },
 });
-
-export default config;
