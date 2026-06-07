5c9a0774a3e2e4ffe97b578997a4b69e2b76b75c
tamanna gupta <tamannagupta0097@gmail.com>
2026-06-07 22:35:47 +0530

    fixed json

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  server: {
+    allowedHosts: "all",
+  },
   tanstackStart: {
     server: { entry: "server" },
     nitro: {
